225a31d3b4189f227864840b00ea6c4e9ef91197
Miri Wasserman <miriwerner1@gmail.com>
2023-03-21 12:30:59 +0200

    fix

--- Sqlinq_test/src/app/login/login.component.ts
+++ Sqlinq_test/src/app/login/login.component.ts
@@ -18,7 +18,7 @@ export class LoginComponent {
   upercaseLetterIndex: number = 0;
   lowercaseLetter: boolean = false;
   lowercaseLetterIndex: number = 0;
-  Hebrew: boolean = false;
+  Hebrew: boolean = true;
   digit: boolean = false;
   digitIndex: number = 0;
   constructor(private loginService: loginService, private router: Router) { }
@@ -45,6 +45,12 @@ export class LoginComponent {
       );
     }
   }
+  checkEmailValaid(){
+    let value = this.loginForm.get('email')?.value;
+    if (value?.match(/[\u05D0-\u05EA]/))
+    this.Hebrew = true;
+    else this.Hebrew = false;
+  }
   checkValidaation() {
     let value = this.loginForm.get('password')?.value;
     if (value?.match(/[\u05D0-\u05EA]/))
@@ -62,57 +68,6 @@ export class LoginComponent {
   }
 
 
-  maskPassword(event: Event) {
-    const inputElement1 = event.target as HTMLInputElement;
-    let val = inputElement1.value;
-
-    if (val.length != this.myLetter) {
-      const inputElement = event.target as HTMLInputElement;
-      let value = inputElement.value;
-      if (value.match(/[\u05D0-\u05EA]/))
-        this.Hebrew = true;
-      if (value.match(/\d/))
-        this.digit = true;
-      if (value.match(/[A-Z]/))
-        this.upercaseLetter = true;
-      if (value.match(/[a-z]/))
-        this.lowercaseLetter = true;
-      this.maskedValue = '';
-      for (let i = 0; i < value.length; i++) {
-        this.maskedValue += '*';
-      }
-      if (inputElement.getAttribute('data-real-value') != undefined && inputElement.getAttribute('data-real-value') != null) {
-        if (value.length < parseInt(inputElement.getAttribute('data-real-value')?.length.toString() || '0')) {
-          this.Hebrew = false;
-          this.lowercaseLetter = false;
-          this.upercaseLetter = false;
-          this.digit = false;
-          if (inputElement.getAttribute('data-real-value') ?? ''?.slice(0, value.length) !== value) {
-            let num = value.length - 1;
-            var data = inputElement.getAttribute('data-real-value') ?? ''?.slice(0, num as number);
-            inputElement.setAttribute('data-real-value', data);
-            console.log(inputElement.getAttribute('data-real-value'));
-            value = inputElement.getAttribute('data-real-value') ?? '';
-          }
-          if (value.match(/[\u05D0-\u05EA]/))
-            this.Hebrew = true;
-          if (value.match(/\d/))
-            this.digit = true;
-          if (value.match(/[A-Z]/))
-            this.upercaseLetter = true;
-          if (value.match(/[a-z]/))
-            this.lowercaseLetter = true;
-        }
-        else if (inputElement.getAttribute('data-real-value') != null)
-          value = inputElement.getAttribute('data-real-value') + value.slice(-1);
-
-      }
-      inputElement.setAttribute('data-real-value', value);
-      inputElement.value = this.maskedValue;
-      this.loginForm.get('password')?.patchValue(inputElement.getAttribute('data-real-value') ?? '');
-      this.myLetter = val.length;
-    }
-    else return;
-  }
+  
 
 }
